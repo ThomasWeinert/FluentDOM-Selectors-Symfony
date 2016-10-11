47f21db065c6a5fdac1be357a80e6b2cd5ede4d9
Thomas Weinert <thomas@weinert.info>
2016-10-11 11:01:56 +0200

    Update symfony/css-selector dependency to version 3, use new interface

--- src/Transformer.php
+++ src/Transformer.php
@@ -2,18 +2,13 @@
 
 namespace FluentDOM\Symfony\CssSelector {
 
-  use FluentDOM\Xpath\Transformer as TransformerInterface;
-  use Symfony\Component\CssSelector\CssSelector as CssSelector;
+  use Symfony\Component\CssSelector\CssSelectorConverter;
 
   class Transformer implements \FluentDOM\Xpath\Transformer {
 
     public function toXpath($selector, $contextMode = self::CONTEXT_CHILDREN, $isHtml = FALSE) {
-      if ($isHtml) {
-        CssSelector::enableHtmlExtension();
-      } else {
-        CssSelector::disableHtmlExtension();
-      }
-      $result = CssSelector::toXpath($selector);
+      $converter = new CssSelectorConverter($isHtml);
+      $result = $converter->toXPath($selector);
       switch ($contextMode) {
       case self::CONTEXT_DOCUMENT :
         $result = '//'.$result;
